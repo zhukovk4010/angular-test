f95f0bbe16212c1666197f9f6ab53306aac55d8f
Zhukov Kirill <zhukovk4010@gmail.com>
2024-08-19 19:38:33 +0300

    add signal button in login page

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AuthService} from "../../auth/auth.service";
 import {Router} from "@angular/router";
@@ -15,6 +15,8 @@ import {Router} from "@angular/router";
 export class LoginPageComponent {
   authService = inject(AuthService);
   router = inject(Router);
+
+  isPasswordVisible = signal<boolean>(false);
 
   form = new FormGroup({
     username: new FormControl(null, Validators.required),
