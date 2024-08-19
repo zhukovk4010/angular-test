9c19ea5a7550fb017f90865832025a234d7013a1
Zhukov Kirill <zhukovk4010@gmail.com>
2024-08-19 20:25:41 +0300

    add sidebar menu

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import {SvgIconComponent} from "../svg-icon/svg-icon.component";
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    SvgIconComponent
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+  menuItems = [
+    {
+      label: 'Моя страница',
+      icon: 'home',
+      link: ''
+    },
+    {
+      label: 'Чаты',
+      icon: 'chats',
+      link: '/chats'
+    },
+    {
+      label: 'Поиск',
+      icon: 'search',
+      link: '/search'
+    },
+  ]
 }
---
+++ src/app/common-ui/svg-icon/svg-icon.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'svg[icon]',
+  standalone: true,
+  imports: [],
+  template: `<svg:use [attr.href]="href"></svg:use>`,
+  styles: ['']
+})
+export class SvgIconComponent {
+  @Input() icon = '';
+  get href() {
+    return `/assets/icons/${this.icon}.svg#${this.icon}`;
+  }
+}
