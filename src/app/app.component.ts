de4ef152c410dd33da8bc24a8b5fa08d04393ccd
Zhukov Kirill PC <zhukovk4010@gmail.com>
2025-01-09 23:22:47 +0300

    update project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {ProfileCardComponent} from "./common-ui/profile-card/profile-card.component";
-import {ProfileService} from "./data/services/profile.service";
-import {IProfile} from "./data/interfaces/profile.intarface";
+import {Component} from '@angular/core';
+import { ProfileCardComponent } from "../components/dummies/profile-card/profile-card.component";
+// import { RouterOutlet } from '@angular/router';
+// import {ProfileCardComponent} from "./common-ui/profile-card/profile-card.component";
+// import {ProfileService} from "./data/services/profile.service";
+// import {IProfile} from "./data/interfaces/profile.intarface";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,4 @@
 import {Routes} from '@angular/router';
-import {SearchPageComponent} from "./pages/search-page/search-page.component";
-import {LoginPageComponent} from "./pages/login-page/login-page.component";
-import {LayoutComponent} from "./common-ui/layout/layout.component";
-import {ProfilePageComponent} from "./pages/profile-page/profile-page.component";
-import {canActivateAuth} from "./auth/access.guard";
 
-export const routes: Routes = [
-  {
-    path: '', component: LayoutComponent, children: [
-      {path: '', component: SearchPageComponent},
-      {path: 'profile', component: ProfilePageComponent},
-    ],
-    canActivate: [canActivateAuth]
-  },
-  {path: 'login', component: LoginPageComponent}
-];
+
+export const routes: Routes = [];
