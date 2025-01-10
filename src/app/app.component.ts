a41b0b465ce8a9ce746ad260a5ff36786c7b1a79
Zhukov Kirill PC <zhukovk4010@gmail.com>
2025-01-10 18:52:07 +0300

    Delete json pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import {Component, inject} from '@angular/core';
 import { ProfileCardComponent } from "../components/dummies/profile-card/profile-card.component";
 import { ProfileService } from '../services/profile.service';
-import { JsonPipe } from '@angular/common';
 import { IProfile } from '../models/api/profile.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ProfileCardComponent , JsonPipe],
+  imports: [ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
